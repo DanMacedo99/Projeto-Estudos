91902e8ce0d9869bc08691d43f557f9239639075
Danilo Macedo <danilomsp6@gmail.com>
2022-11-23 12:19:47 -0300

    feature/new form and state

--- Aulas/src/app/pages/login/Login.tsx
+++ Aulas/src/app/pages/login/Login.tsx
@@ -1,17 +1,45 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom"
 //pela atualização do react-dom invés de useHistory utilizamos useNavigate
 export const Login = () => {
-
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
     const navigate = useNavigate();
     //const history = useHistory();
+
+    const handleEntrar = () => {
+        console.log(email)
+        console.log(senha)
+    }
     const handleClick = () => {
         navigate("/pagina-inicial")
     // history.push("/pagina-inicial")
     }
     return (
-        <div>
+        <body>
+            <div>
             <button onClick={handleClick}>Página inicial</button>
-            <p>Login</p>
-        </div>
-    )
-}
+            </div>
+
+            <div>
+                <form>
+                    <label>
+                        <span>Email</span>
+                        <input value={email} onChange={evento => setEmail(evento.target.value)}/>
+                    </label>
+
+
+                    <label>
+                        <span>Senha</span>
+                        <input type="password" value={senha} onChange={evento => setSenha(evento.target.value)} />
+                    </label>
+
+
+                    <button type="button" onClick={handleEntrar}>
+                        <p>Entrar</p>
+                    </button>
+                </form>
+            </div>
+        </body>
+     
+)}
