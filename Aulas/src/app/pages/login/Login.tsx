eb33261e08b152079e108ff45ff528084518c90d
Danilo Macedo <danilomsp6@gmail.com>
2022-11-23 16:43:14 -0300

    feature/add useRef

--- Aulas/src/app/pages/login/Login.tsx
+++ Aulas/src/app/pages/login/Login.tsx
@@ -1,22 +1,41 @@
-import { useState } from "react";
+import { useRef, useCallback, useEffect, useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom"
 //pela atualização do react-dom invés de useHistory utilizamos useNavigate
+
 export const Login = () => {
+    const inputPasswordRef = useRef<HTMLInputElement>(null);
+
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
     const navigate = useNavigate();
     //const history = useHistory();
 
-    const handleEntrar = () => {
+    // useEffect(() => {
+    //     console.log(email)
+    // }, [email]);
+    
+    // useEffect(() => {
+    //     console.log(senha)
+    // }, [senha]);
+
+    // const emailLength = useMemo(() => {
+    //     console.log("EXECUTOU")
+    //     return email.length * 1000;
+    // }, [email.length]);
+
+    const handleEntrar = useCallback(() => {
         console.log(email)
         console.log(senha)
-    }
+    }, [email, senha]);
+
     const handleClick = () => {
         navigate("/pagina-inicial")
     // history.push("/pagina-inicial")
     }
     return (
-        <body>
+        <div>
+            {/* <p>Quantidade de digitações x 1.000: {emailLength}</p> */}
+            
             <div>
             <button onClick={handleClick}>Página inicial</button>
             </div>
@@ -25,13 +44,21 @@ export const Login = () => {
                 <form>
                     <label>
                         <span>Email</span>
-                        <input value={email} onChange={evento => setEmail(evento.target.value)}/>
+                        <input 
+                        value={email}
+                        onChange={evento => 
+                        setEmail(evento.target.value)} 
+                        onKeyDown={evento => evento.key === 'Enter' ? inputPasswordRef.current?.focus() : undefined} />
                     </label>
 
 
                     <label>
                         <span>Senha</span>
-                        <input type="password" value={senha} onChange={evento => setSenha(evento.target.value)} />
+                        <input 
+                        type="password" 
+                        ref={inputPasswordRef} 
+                        value={senha} 
+                        onChange={evento => setSenha(evento.target.value)} />
                     </label>
 
 
@@ -40,6 +67,6 @@ export const Login = () => {
                     </button>
                 </form>
             </div>
-        </body>
+        </div>
      
 )}
